901aa79750d0ca17326f796f22399f0854809bc5
alam-faisal <mfalam2@illinois.edu>
2025-04-07 10:51:41 -0600

    top_entropy() again

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from qaravan.tensorQ import op_action
-from qaravan.core import ChiralHeisenberg
+from qaravan.tensorQ import op_action, rdm_from_sv
+from qaravan.core import ChiralHeisenberg, vN_entropy
 import numpy as np
 from scipy.sparse.linalg import LinearOperator
 from primme import eigsh
@@ -30,3 +30,21 @@ def ground(ham, ncv=10):
     H_linop = LinearOperator(shape=(dim, dim), matvec=mv, dtype=np.complex128)
     vals, vecs = eigsh(H_linop, k=1, which='SA', ncv=ncv, maxiter=dim)  # 'SA' = smallest algebraic
     return vals[0], vecs[:, 0]
+
+def top_entropy(gstate, regions): 
+    rdm_A = rdm_from_sv(gstate, regions[0])
+    rdm_B = rdm_from_sv(gstate, regions[1])
+    rdm_C = rdm_from_sv(gstate, regions[2])
+    rdm_AB = rdm_from_sv(gstate, regions[0]+regions[1])
+    rdm_BC = rdm_from_sv(gstate, regions[1]+regions[2])
+    rdm_AC = rdm_from_sv(gstate, regions[0]+regions[2])
+    rdm_ABC = rdm_from_sv(gstate, regions[0]+regions[1]+regions[2])
+
+    sA = vN_entropy(rdm_A)
+    sB = vN_entropy(rdm_B)
+    sC = vN_entropy(rdm_C)
+    sAB = vN_entropy(rdm_AB)
+    sBC = vN_entropy(rdm_BC)
+    sAC = vN_entropy(rdm_AC)
+    sABC = vN_entropy(rdm_ABC)
+    return np.real(sA + sB + sC - sAB - sBC - sAC + sABC)
